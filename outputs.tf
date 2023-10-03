cf23d2e595fd736b27dc8cd2ffda85f754fbeb0b
Kamlesh <koladiya_111@yahoo.com>
2023-10-03 15:53:10 +0000

    aws s3 object upload

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "bucket_name" {
   description = "Bucket name for our static website hosting"
   value = module.terrahouse_aws.bucket_name
 }
+
+output "s3_url" {
+  value = module.terrahouse_aws.website_endpoint
+}
