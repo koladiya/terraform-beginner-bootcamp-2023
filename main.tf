fc3cee451024376a7e59ef7ada0e77a3842d43d8
Kamlesh <koladiya_111@yahoo.com>
2023-10-03 17:59:35 +0200

    aws s3 object upload (#18)

--- main.tf
+++ main.tf
@@ -12,4 +12,6 @@ module "terrahouse_aws" {
   source = "./modules/terrahouse_aws"
   user_uuid = var.user_uuid
   bucket_name = var.bucket_name
+  index_html_filepath = "${path.root}/public/index.html"
+  error_html_filepath = "${path.root}/public/error.html"
 }
