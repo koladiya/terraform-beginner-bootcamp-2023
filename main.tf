3868220efa83a9e47b2ccaff0f7c18c6c5cf33c5
Kamlesh <koladiya_111@yahoo.com>
2023-10-03 11:46:10 +0000

    root module modification

--- main.tf
+++ main.tf
@@ -1,34 +1,3 @@
-
-terraform {
-  cloud {
-    organization = "kk-test1"
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-
-provider "aws" {
-  # Configuration options
-
-}
-
-provider "random" {
-  # Configuration options
-}
-
-
 resource "random_string" "bucket_name" {
   length           = 32
   upper            = false
@@ -37,16 +6,12 @@ resource "random_string" "bucket_name" {
   override_special = ""
 }
 
-output "random_bucket_name" {
-    value = random_string.bucket_name.result
-}
 
 resource "aws_s3_bucket" "example" {
   bucket = random_string.bucket_name.result
 
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    UserUuid    = var.user_uuid
   }
 }
 
