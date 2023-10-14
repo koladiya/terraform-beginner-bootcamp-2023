ed4c7fda4c5a2bae95defa1c48e61226f7f1b46e
Kamlesh <koladiya_111@yahoo.com>
2023-10-14 15:42:46 +0200

    terratown test done (#33)

--- main.tf
+++ main.tf
@@ -16,33 +16,32 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  # endpoint = "http://localhost:4567/api"
+  endpoint = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token = var.terratowns_access_token
 }
 
 
-# module "terrahouse_aws" {
-#   source = "./modules/terrahouse_aws"
-#   user_uuid = var.user_uuid
-#   bucket_name = var.bucket_name
-#   index_html_filepath = var.index_html_filepath # "${path.root}/public/index.html"
-#   error_html_filepath = var.error_html_filepath # "${path.root}/public/error.html"
-#   assets_path = var.assets_path
-#   content_version = var.content_version
-# }
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.teacherseat_user_uuid
+  # bucket_name = var.bucket_name
+  index_html_filepath = var.index_html_filepath # "${path.root}/public/index.html"
+  error_html_filepath = var.error_html_filepath # "${path.root}/public/error.html"
+  assets_path = var.assets_path
+  content_version = var.content_version
+}
 
 
 resource "terratowns_home" "home" {
-  name = "How to play Arcanum in 2023!"
+  name = "How to make Dabeli?"
   description = <<DESCRIPTION
-Arcanum is a game from 2001 that shipped with alot of bugs.
-Modders have removed all the originals making this game really fun
-to play (despite that old look graphics). This is my guide that will
-show you how to play arcanum without spoiling the plot.
+Dabeli is a very popular snack from the Gujarati cuisine and originated from the Kutch or Kachchh region of Gujarat state in India. This dish is also commonly known as Kutchi Dabeli or Kachchhi Dabeli.
+Dabeli is a fantastic spicy snack made by mixing boiled potatoes with a special Dabeli Masala and putting the mixture between the Ladi pav (burger bun) and served with delicious chutneys made from tamarind, date, garlic, red chillies etc and garnished with pomegranate and roasted pea-nuts.
 DESCRIPTION
   #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
-  town = "gamers-grotto"
+  domain_name =  module.terrahouse_aws.cloudfront_url
+  town = "cooker-cove"
   content_version = 1
 }
--- variables.tf
+++ variables.tf
@@ -1,23 +1,32 @@
-# variable "user_uuid" {
-#  type = string
-# }
+variable "terratowns_endpoint" {
+  type = string
+}
+
+variable "teacherseat_user_uuid" {
+ type = string
+}
+
+variable "terratowns_access_token" {
+ type = string
+}
 
 # variable "bucket_name" {
 #  type = string
 # }
 
-# variable "index_html_filepath" {
-#   type = string
-# }
+variable "index_html_filepath" {
+  type = string
+}
 
-# variable "error_html_filepath" {
-#   type = string
-# }
+variable "error_html_filepath" {
+  type = string
+}
 
-# variable "content_version" {
-#   type        = number
-# }
+variable "content_version" {
+  type        = number
+}
 
-# variable "assets_path" {
-#   type = string
-# }
+variable "assets_path" {
+  type = string
+}
+
